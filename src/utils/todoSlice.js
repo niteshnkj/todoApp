6e0ba419569d09e49b78868e89f7b6732bdcc125
Nitesh Kumar Jha <niteshkrjha.nkj@gmail.com>
2024-07-12 02:19:02 +0530

    added comments

--- src/utils/todoSlice.js
+++ src/utils/todoSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const savedTodos = localStorage.getItem("todos");
+//if there is any saved todos in the local storage then it will be used as initial state or else it will be an empty array
 const initialState = savedTodos ? JSON.parse(savedTodos) : [];
 
 const todoSlice = createSlice({
@@ -9,17 +10,22 @@ const todoSlice = createSlice({
     todos: initialState,
   },
   reducers: {
+    //for pushing the todo to the state
     addTodo: (state, action) => {
       state.todos.push(action.payload);
       localStorage.setItem("todos", JSON.stringify(state.todos));
     },
+    //for removing the todo from the state
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
       localStorage.setItem("todos", JSON.stringify(state.todos));
     },
+    //for editing the todo
     editTodo: (state, action) => {
       const { id, newValue } = action.payload;
+
       const todo = state.todos.find((todo) => todo.id === id);
+      //if the todo is found then the text will be updated
       if (todo) {
         todo.text = newValue;
         localStorage.setItem("todos", JSON.stringify(state.todos));
